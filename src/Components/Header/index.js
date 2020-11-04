d1167ffa216205d8981564a9cc52f27e4e82a826
AbdoWahba <abdo.tw20154@gmail.com>
2020-11-04 03:55:01 +0200

    implementing header component

---
+++ src/Components/Header/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.scss';
+function Header() {
+  return (
+    <div className='header'>
+      <h6 className='header__title'>Transportation</h6>
+    </div>
+  );
+}
+
+export default Header;
