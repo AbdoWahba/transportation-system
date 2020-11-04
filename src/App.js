5f43edfaf80cbb0c34554b8b87a0f31d828871b8
AbdoWahba <abdo.tw20154@gmail.com>
2020-11-04 15:43:44 +0200

    adding components to app to test it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.scss';
 import Header from './Components/Header';
+import CheckBox from './Components/CheckBox';
+import { useState } from 'react';
+import Table from './Components/Table';
 
 function App() {
   return (
     <div className='App'>
       <Header />
+      <main>
+        <section className='main-section'>
+          <Table />
+        </section>
+      </main>
     </div>
   );
 }
