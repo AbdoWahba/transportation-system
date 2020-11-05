10d55a2686667a4ab1e33cf3817d74f8ae6ffe53
AbdoWahba <abdo.tw20154@gmail.com>
2020-11-05 23:36:48 +0200

    configure routing for app calling lookups at strarting

--- src/App.js
+++ src/App.js
@@ -1,18 +1,34 @@
 import './App.scss';
 import Header from './Components/Header';
-import Table from './Components/Table';
 import CompaniesList from './Pages/CompaniesList';
-
+import { Route, BrowserRouter } from 'react-router-dom';
+import TransportationsFormAdd from './Pages/TransportationsForm/TransportationsFormAdd';
+import { useDispatch } from 'react-redux';
+import { useEffect } from 'react';
+import { getCountries, getVihcle } from './store/actions';
 function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getVihcle());
+    dispatch(getCountries());
+  }, []);
+
   return (
-    <div className='App'>
-      <Header />
-      <main>
-        <section className='main-section'>
-          <CompaniesList />
-        </section>
-      </main>
-    </div>
+    <BrowserRouter>
+      <div className='App'>
+        <Header />
+        <main>
+          <section className='main-section'>
+            <Route exact path='/' component={CompaniesList} />
+            <Route
+              exact
+              path='/companyDetails/add'
+              component={TransportationsFormAdd}
+            />
+          </section>
+        </main>
+      </div>
+    </BrowserRouter>
   );
 }
 
